bfa58d523e98ba71ac262a153475a3cbbd54d410
ijaz35 <ijaz.hossain1@gmail.com>
2023-02-25 10:53:20 +0600

    countries displayed  to UI

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-console.log('script added');
+const loadCountries = async () => {
+    const res = await fetch('https://restcountries.com/v3.1/all');
+    const data = await res.json();
+    displayCountries(data);
+}
+const displayCountries = (countries) => {
+    const countriesContainer = document.getElementById('countries-container');
+
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+        <img src="${country.flags.png}">
+        <h3>Name:${country.name.common}</h3>
+        <p>Population:${country.population} </p>
+        `;
+        countriesContainer.appendChild(countryDiv);
+    });
+}
+loadCountries()
